11d057420731c088db110735f054e978e49d3a49
arreglo multidimensional <yl.vasquezm@uea.edu.ec>
2024-03-09 20:31:06 -0500

    (Semana 12) Tarea: Iteración de arreglos multidimensionales con bucles anidados

--- Iteración de arreglos multidimensionales con bucles anidados.py
+++ Iteración de arreglos multidimensionales con bucles anidados.py
@@ -1,10 +1,8 @@
-# Crear una matriz 3D para almacenar datos de temperaturas
-# Primera dimensión: Ciudades (3 ciudades)
-# Segunda dimensión: Semanas (3 semanas)
-# Tercera dimensión: Días de la semana (7 días)
+# Datos de las ciudades y temperaturas
 temperaturas = [
-    [   # Ciudad Lago Agrio 1
-        [   # Semana 1
+    [   # Ciudad LAGO AGRIO
+        {"Ciudad": "LAGO AGRIO"},
+        [
             {"day": "Lunes", "temp": 25},
             {"day": "Martes", "temp": 15},
             {"day": "Miércoles", "temp": 18},
@@ -13,7 +11,7 @@ temperaturas = [
             {"day": "Sábado", "temp": 22},
             {"day": "Domingo", "temp": 23}
         ],
-        [   # Semana 2
+        [
             {"day": "Lunes", "temp": 20},
             {"day": "Martes", "temp": 17},
             {"day": "Miércoles", "temp": 16},
@@ -22,7 +20,7 @@ temperaturas = [
             {"day": "Sábado", "temp": 27},
             {"day": "Domingo", "temp": 29}
         ],
-        [   # Semana 3
+        [
             {"day": "Lunes", "temp": 14},
             {"day": "Martes", "temp": 18},
             {"day": "Miércoles", "temp": 19},
@@ -30,11 +28,11 @@ temperaturas = [
             {"day": "Viernes", "temp": 28},
             {"day": "Sábado", "temp": 30},
             {"day": "Domingo", "temp": 32}
-        ],
-
+        ]
     ],
-    [   # Ciudad Francisco de Orellana 2
-        [   # Semana 1
+    [   # Ciudad JOYA DE LOS SACHAS
+        {"Ciudad": "JOYA DE LOS SACHAS"},
+        [
             {"day": "Lunes", "temp": 25},
             {"day": "Martes", "temp": 26},
             {"day": "Miércoles", "temp": 28},
@@ -43,7 +41,7 @@ temperaturas = [
             {"day": "Sábado", "temp": 32},
             {"day": "Domingo", "temp": 33}
         ],
-        [   # Semana 2
+        [
             {"day": "Lunes", "temp": 34},
             {"day": "Martes", "temp": 36},
             {"day": "Miércoles", "temp": 31},
@@ -52,7 +50,7 @@ temperaturas = [
             {"day": "Sábado", "temp": 48},
             {"day": "Domingo", "temp": 19}
         ],
-        [   # Semana 3
+        [
             {"day": "Lunes", "temp": 23},
             {"day": "Martes", "temp": 25},
             {"day": "Miércoles", "temp": 27},
@@ -60,11 +58,11 @@ temperaturas = [
             {"day": "Viernes", "temp": 29},
             {"day": "Sábado", "temp": 30},
             {"day": "Domingo", "temp": 34}
-        ],
-
+        ]
     ],
-    [   # Ciudad Joya de los Sachas 3
-        [   # Semana 1
+    [   # Ciudad FRANCISCO DE ORELLANA
+        {"Ciudad": "FRANCISCO DE ORELLANA"},
+        [
             {"day": "Lunes", "temp": 12},
             {"day": "Martes", "temp": 14},
             {"day": "Miércoles", "temp": 18},
@@ -73,7 +71,7 @@ temperaturas = [
             {"day": "Sábado", "temp": 26},
             {"day": "Domingo", "temp": 29}
         ],
-        [   # Semana 2
+        [
             {"day": "Lunes", "temp": 27},
             {"day": "Martes", "temp": 19},
             {"day": "Miércoles", "temp": 24},
@@ -82,7 +80,7 @@ temperaturas = [
             {"day": "Sábado", "temp": 84},
             {"day": "Domingo", "temp": 81}
         ],
-        [   # Semana 3
+        [
             {"day": "Lunes", "temp": 25},
             {"day": "Martes", "temp": 23},
             {"day": "Miércoles", "temp": 22},
@@ -90,15 +88,28 @@ temperaturas = [
             {"day": "Viernes", "temp": 19},
             {"day": "Sábado", "temp": 18},
             {"day": "Domingo", "temp": 15}
-        ],
-
+        ]
     ]
 ]
 
+
+def calcular_promedio(divisor, dividendo):
+    return round(divisor / dividendo)
+
 # Calcular el promedio de temperaturas para cada ciudad y semana
+no_ciudad = 0
 for ciudad in temperaturas:
-    for semana in ciudad:
-        suma = 0
-        for dia in semana:
-            suma += dia['temp']
-        print(suma)
+    no_ciudad = no_ciudad + 1
+    print(f'CIUDAD No.{no_ciudad}')
+    no_semana = 0
+    suma_promedio = 0
+
+    for semanas in ciudad[1:]:
+        no_semana += 1
+        suma = sum(dia['temp'] for dia in semanas)
+        promedio_semana = calcular_promedio(suma, len(semanas))
+        print(f'El promedio semana No.{no_semana} es: {promedio_semana}')
+        suma_promedio += promedio_semana
+
+    promedio_ciudad = calcular_promedio(suma_promedio, len(ciudad) - 1)
+    print(f'El promedio mensual es: {promedio_ciudad}')
